c44127578ea45005a61731e1ff638dec5e01febc
ikanago <28985004+ikanago@users.noreply.github.com>
2020-04-13 21:11:55 +0900

    ✨ Fundamental functions of BinaryTree

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,8 @@ module Main where
 import           BinaryTree
 
 main :: IO ()
-main = print $ singleton 6
+main = do
+    print tree
+    print $ search 5 tree
+    print $ search 11 tree
+    where tree = newBinaryTree [5, 3, 7, 2, 4, 1, 9, 10, 6]
--- src/BinaryTree.hs
+++ src/BinaryTree.hs
@@ -2,5 +2,18 @@ module BinaryTree where
 
 data BinaryTree a = Nil | Node a (BinaryTree a) (BinaryTree a) deriving (Show)
 
-singleton :: (Ord a) => a -> BinaryTree a
 singleton a = Node a Nil Nil
+
+newBinaryTree :: (Ord a) => [a] -> BinaryTree a
+newBinaryTree = foldr insert Nil . reverse
+
+insert :: (Ord a) => a -> BinaryTree a -> BinaryTree a
+insert x Nil = Node x Nil Nil
+insert x (Node a lhs rhs) | x < a  = Node a (insert x lhs) rhs
+                             | x == a = Node x lhs rhs
+                             | x > a  = Node a lhs (insert x rhs)
+
+search x Nil = False
+search x (Node a lhs rhs) | x < a  = search x lhs
+                             | x == a = True
+                             | x > a  = search x rhs
